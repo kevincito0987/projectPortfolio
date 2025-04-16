0713980ddc0b746c1599f9955fffdabb6da1641f
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-16 08:40:28 -0500

    perf: :zap: Listo, ya hace scroll con web components de js.

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,17 @@
 console.log("Scroll esta funcionando");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".center-Nav a");
+
+    links.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault(); // Evita el comportamiento predeterminado
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+            }
+        });
+    });
+});
